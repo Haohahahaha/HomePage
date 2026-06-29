34d1a449f627078611bbacc9230c3fc53027ea98
Zhang Haorui <1259203802@qq.com>
2026-06-29 10:40:00 +0800

    2026-06-29_10:40:00 update

--- docs/index.md
+++ docs/index.md
@@ -16,3 +16,7 @@
 	</div>
 
     - 这是我的个人站，其中记录着我对世界的探索……欢迎[交流](https://haohaha.cn/links/)！
+
+!!! warning "我的日程"
+
+    <iframe src="https://calendar.google.com/calendar/embed?src=telppbf9lu1m20ediudg35ao07jupbjp%40import.calendar.google.com&ctz=Etc%2FGMT&mode=WEEK&hl=zh_CN" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
---
+++ scripts/fix_ics.py
@@ -0,0 +1,29 @@
+import requests
+
+ICS_URL = "https://outlook.live.com/owa/calendar/00000000-0000-0000-0000-000000000000/7b3272d9-59ae-4b21-a6ee-93055d9dabed/cid-7D0F5EF787CF53A7/calendar.ics"
+
+text = requests.get(ICS_URL).text
+
+# 修复微软私有时区
+text = text.replace(
+    "TZID:Customized Time Zone",
+    "TZID:Asia/Shanghai"
+)
+
+text = text.replace(
+    "TZID=Customized Time Zone",
+    "TZID=Asia/Shanghai"
+)
+
+# 可选：补一个标准时区声明
+text = text.replace(
+    "BEGIN:VTIMEZONE\nTZID:Asia/Shanghai",
+    """BEGIN:VTIMEZONE
+TZID:Asia/Shanghai
+X-LIC-LOCATION:Asia/Shanghai"""
+)
+
+with open("docs/calendar.ics", "w", encoding="utf-8") as f:
+    f.write(text)
+
+print("ICS 已更新")
